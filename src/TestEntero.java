91e4b49c76eff438077242b420eb3ebd88afceea
Ricardo Cantó Abad <rcanto@users.noreply.github.com>
2018-11-09 13:48:03 +0100

    Update TestEntero.java

--- src/TestEntero.java
+++ src/TestEntero.java
@@ -3,12 +3,13 @@
 import junit.framework.TestCase;
 public class TestEntero extends TestCase{
 	//@Test
-	public void testSimple() {
-    Entero e1=new Entero(4);
-    Entero e2=new Entero(5);
-		assertEquals(true, e1.esPar());
-    assertEquals(false, e2.esPar());
-		}
-	
+	public void test1() {
+    		Entero e1=new Entero(4);
+    		assertEquals(true, e1.esPar());
+    }
+	public void test2() {
+    		Entero e2=new Entero(5);
+    		assertEquals(false, e2.esPar());
+    }
 
 }
